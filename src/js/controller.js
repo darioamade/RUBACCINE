c3adc88bd924334302bff89f8eb27a5a3775b969
Dario Amade <dario_isd@outlook.com>
2021-01-31 13:48:56 +0000

    added notes and sizes

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,40 +1,40 @@
-// const hero = document.querySelector('.hero');
-// const hoveruv = document.querySelector('.hoveruv');
-// const search = document.querySelector('.search');
-// const overlay = document.querySelector('.overlay-1');
-// const emailThick = document.querySelector('.text-thiny');
-// const signIn = document.querySelector('#footer-newsletter-email');
+const hero = document.querySelector('.hero');
+const hoveruv = document.querySelector('.hoveruv');
+const search = document.querySelector('.search');
+const overlay = document.querySelector('.overlay-1');
+const emailThick = document.querySelector('.text-thiny');
+const signIn = document.querySelector('#footer-newsletter-email');
 
-// const footer = document.querySelectorAll('.nav__list-footer-2');
-// const iconFooterOpen = document.querySelector('#dario');
+const footer = document.querySelectorAll('.nav__list-footer-2');
+const iconFooterOpen = document.querySelector('#dario');
 
-// const dual = function () {
-//   search.classList.toggle('hidden');
-//   overlay.classList.remove('hidden');
-// };
+const dual = function () {
+  search.classList.toggle('hidden');
+  overlay.classList.remove('hidden');
+};
 
-// hoveruv.addEventListener('mouseenter', dual);
-// signIn.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   emailThick.classList.remove('hidden');
-// });
+hoveruv.addEventListener('mouseenter', dual);
+signIn.addEventListener('click', function (e) {
+  e.preventDefault();
+  emailThick.classList.remove('hidden');
+});
 
-// const openFooter = function(){
-//   console.log('dskuvilnd');
-// }
+const openFooter = function(){
+  console.log('dskuvilnd');
+}
 
-// const amade = document.querySelector('.nav__list-footer');
+const amade = document.querySelector('.nav__list-footer');
 
-// iconFooterOpen.addEventListener('click', function (e){
-//   const clicked = e.target.closest('.nav__list-footer-2')
+iconFooterOpen.addEventListener('click', function (e){
+  const clicked = e.target.closest('.nav__list-footer-2')
 
-//   clicked.classList.add('operations__tab--active');
-// })
+  clicked.classList.add('operations__tab--active');
+})
 
-// iconFooterOpen.addEventListener('click', function(e){
-//   const
+iconFooterOpen.addEventListener('click', function(e){
+  const
 
-// })
+})
 
 /* 
 
